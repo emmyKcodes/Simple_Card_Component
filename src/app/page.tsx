d0ba25a85f8ddc2fb2345d5e90cbb7e8fd44a02d
Emmanuel Kehinde <ekehinde12345@gmail.com>
2025-07-08 14:13:37 +0100

    Final Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,11 @@ const UpdatesCard = () => {
   return (
     <div className="min-h-screen bg-gray-700 flex items-center justify-center p-4">
       <div className="p-2 rounded-3xl bg-gradient-to-br from-gray-300 to-black shadow-2xl">
-        <div className="relative w-[28rem] h-[28rem] bg-gradient-to-b from-neutral-900 from-0% via-orange-600 via-25% to-neutral-900 to-50% rounded-2xl p-8 flex flex-col items-center justify-center text-white overflow-hidden">
+        <div className="relative max-w-[35rem] max-h-[28rem] bg-gradient-to-b from-neutral-900 from-0% via-orange-600 via-25% to-neutral-900 to-50% rounded-2xl p-8 flex flex-col items-center justify-center text-white overflow-hidden">
           <div className="absolute w-48 h-48 bg-orange-600 opacity-30 rounded-full blur-2xl top-6 left-1/2 -translate-x-1/2"></div>
 
-          <div className="text-center mb-6 relative z-10">
-            <h1 className="text-4xl font-light mb-4 tracking-widest leading-relaxed">
+          <div className="text-center mb-2 relative z-10">
+            <h1 className="text-2xl md:text-4xl font-light mb-4 tracking-widest leading-relaxed">
               48-Hour Updates
             </h1>
             <p className=" font-light leading-loose tracking-wide max-w-70 mx-auto relative left-[-32px] ">
